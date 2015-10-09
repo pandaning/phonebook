b65a8d77308688d9a1df9acffd15bc3723a85893
Yuning Lin <pandaning9112@gmail.com>
2015-10-09 15:58:57 +0800

    Use HashTable.
    This is the first version I try to implement Hash Table, every string
    is added by its ASCII code and put into different hash table.

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
@@ -12,4 +12,76 @@ entry *findName(char lastname[], entry *pHead)
 entry *append(char lastName[], entry *e)
 {
     return NULL;
+}
+
+void init()
+{
+	for(int i=0;i<20000;i++)
+	{	
+		head[i]=malloc(sizeof(HL));
+		head[i]->next=NULL;
+		ptr[i]=head[i];
+	}
+
+}
+int HashFunc(char *str,int method)//return pos
+{
+	int pos=0;
+	int strLength =  strlen(str);
+	switch(method)
+	{
+		case 1:
+			for(int i=0; i<strLength; i++)
+			{
+				pos+=str[i];
+			}
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+		
+	}
+	return pos;
+}
+int IsFound(char str[])
+{
+	int yes=1;
+	int no=0;
+	int id=HashFunc(str,1);
+	for(HL* tmp=head[id] ; tmp!=NULL ; tmp=tmp->next)
+	{
+		//if(tmp->name==str)
+		if(strcmp(tmp->name , str)==0)
+		{
+			return yes;
+		}
+	}
+	return no;
+}
+void pushHashTable(char *str,int id)
+{
+	ptr[id]->next=malloc(sizeof(HL));
+	ptr[id]=ptr[id]->next;
+	strcpy(ptr[id]->name,str);     //ptr[id]->name=str;
+	ptr[id]->next=NULL;
+}
+
+void FREE()
+{
+puts("Free La");
+	for(int i=0;i<20000;i++)
+	{
+		HL *tmp=head[i];
+		HL *f=tmp;
+		if(tmp->next!=NULL)tmp=tmp->next;
+		for(;tmp->next!=NULL;tmp=tmp->next)
+		{
+			free(f);
+			f=tmp;
+		}
+	}
+
 }
