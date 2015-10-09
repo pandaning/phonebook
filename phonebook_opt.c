89d70ef3e1b57e9bf85f999ba882c40f7c82000f
Yuning Lin <pandaning9112@gmail.com>
2015-10-09 23:04:25 +0800

    Different hash functions
    The size of hash table can change to test the performance.
    
    There are 4 hash functions can choose to test now.
    
    Rename FREE() function to FREE_HashTable(), and use astyle.

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "phonebook_opt.h"
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
@@ -16,72 +17,73 @@ entry *append(char lastName[], entry *e)
 
 void init()
 {
-	for(int i=0;i<20000;i++)
-	{	
-		head[i]=malloc(sizeof(HL));
-		head[i]->next=NULL;
-		ptr[i]=head[i];
-	}
-
+    for(int i=0; i<HashTableSize; i++) {
+        head[i]=malloc(sizeof(HashList));
+        head[i]->next=NULL;
+        ptr[i]=head[i];
+    }
 }
-int HashFunc(char *str,int method)//return pos
+int HashFunc(char *str)//return pos
 {
-	int pos=0;
-	int strLength =  strlen(str);
-	switch(method)
-	{
-		case 1:
-			for(int i=0; i<strLength; i++)
-			{
-				pos+=str[i];
-			}
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		default:
-			break;
-		
-	}
-	return pos;
+    unsigned long long int pos=0;
+    int strLength =  strlen(str);
+    int chooseMethod = method;
+    switch(chooseMethod) {
+    case 1:
+        for(int i=0; i<strLength; i++) {
+            pos+=str[i];
+        }
+        break;
+    case 2:
+        for(int i=0; i<strLength; i++) {
+            pos+=str[i]<<i;
+        }
+        break;
+    case 3:
+        pos=5381;
+        for(int i=0; str[i]; i++) {
+            pos=((pos<<5)+pos)+str[i];
+        }
+        break;
+    case 4:
+        for(int i=0; str[i]; i++) {
+            pos=((pos<<6)+(pos<<16)) - pos + str[i];
+        }
+        break;
+    default:
+        break;
+    }
+    return pos%HashTableSize;
 }
 int IsFound(char str[])
 {
-	int yes=1;
-	int no=0;
-	int id=HashFunc(str,1);
-	for(HL* tmp=head[id] ; tmp!=NULL ; tmp=tmp->next)
-	{
-		//if(tmp->name==str)
-		if(strcmp(tmp->name , str)==0)
-		{
-			return yes;
-		}
-	}
-	return no;
+    int yes=1;
+    int no=0;
+    int id=HashFunc(str);
+    for(HashList* tmp=head[id] ; tmp!=NULL ; tmp=tmp->next) {
+        if(strcmp(tmp->name , str)==0) { //if(tmp->name==str)
+            return yes;
+        }
+    }
+    return no;
 }
 void pushHashTable(char *str,int id)
 {
-	ptr[id]->next=malloc(sizeof(HL));
-	ptr[id]=ptr[id]->next;
-	strcpy(ptr[id]->name,str);     //ptr[id]->name=str;
-	ptr[id]->next=NULL;
+    ptr[id]->next=malloc(sizeof(HashList));
+    ptr[id]=ptr[id]->next;
+    strcpy(ptr[id]->name,str);     //ptr[id]->name=str;
+    ptr[id]->next=NULL;
 }
 
-void FREE()
+void FREE_HashTable()
 {
-puts("Free La");
-	for(int i=0;i<20000;i++)
-	{
-		HL *tmp=head[i];
-		HL *f=tmp;
-		if(tmp->next!=NULL)tmp=tmp->next;
-		for(;tmp->next!=NULL;tmp=tmp->next)
-		{
-			free(f);
-			f=tmp;
-		}
-	}
-
+    for(int i=0; i<HashTableSize; i++) {
+        HashList *tmp=head[i];
+        HashList *f=tmp;
+        if(tmp->next!=NULL)tmp=tmp->next;
+        for(; tmp->next!=NULL; tmp=tmp->next) {
+            free(f);
+            f=tmp;
+        }
+    }
 }
