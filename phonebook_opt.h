ac765de4b2d6a790309b560187112bdc13b7bc8b
Yuning Lin <pandaning9112@gmail.com>
2015-10-10 16:12:06 +0800

    Rename Function and modify parameter
    Rename function name to reduce misleading.
     init to init_Hash_Table,
     pushHashTable to push_Hash_Table,
     FREE_HashTable to Release_Hash_Table.
    
    Modify the parameter in char name[] by 20 to 13, because in the
    word.txt, the length of the max name is 12, so set the parameter to 13
    can save every name correctly.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,36 +6,18 @@
 #define method  2      //Choose which hash function to use.
 #define HashTableSize 69997 //a prime number
 
-/* original version */
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
-
-entry *findName(char lastname[], entry *pHead);
-entry *append(char lastName[], entry *e);
-
 /* Revised version */
 typedef struct  HashList {
-    char name[20];
+    char name[13];
     struct HashList *next;
 } HashList;
 
 HashList *head[HashTableSize];
 HashList *ptr[HashTableSize];
 
-void init();
+void init_Hash_Table();
 int HashFunc(char *str);//return pos;
 int IsFound(char *str);
-void pushHashTable(char *str,int id);
-void FREE_HashTable();
+void push_Hash_Table(char *str,int id);
+void Release_Hash_Table();
 #endif
