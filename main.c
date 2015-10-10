9a98fb5e76e6b769096bb8aa776596ea607e394e
Yuning Lin <pandaning9112@gmail.com>
2015-10-10 16:22:52 +0800

    Use conditional compilation
    Use conditional compilation in main.c so that Makefile can build both
    phonebook_orig and phonebook_opt versions by each correspond #define.
    
    Delete useless dead code.

--- main.c
+++ main.c
@@ -20,6 +20,61 @@ static double diff_in_second(struct timespec t1, struct timespec t2)
     return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
 }
 
+#ifdef HASHFUNCTION
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    int i = 0;
+    char line[MAX_LAST_NAME_SIZE];
+    struct timespec start, end;
+    double cpu_time1, cpu_time2;
+
+    /* check file opening */
+    fp = fopen(DICT_FILE, "r");
+    if (fp == NULL) {
+        printf("cannot open the file\n");
+        return -1;
+    }
+    /*Build head for Hash Table*/
+    init_Hash_Table();
+
+    printf("size of entry : %lu bytes\n", sizeof(HashList));
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    while (fgets(line, sizeof(line), fp)) {
+        while (line[i] != '\0')
+            i++;
+        line[i - 1] = '\0';
+        i = 0;
+        push_Hash_Table(line,HashFunc(line));
+    }
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time1 = diff_in_second(start, end);
+
+    /* close file as soon as possible */
+    fclose(fp);
+
+    /* the given last name to find */
+    char input[MAX_LAST_NAME_SIZE] = "zzzzzzzz";
+
+    /* compute the execution time */
+    clock_gettime(CLOCK_REALTIME, &start);
+    int found=IsFound(input);
+    assert(found==1);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time2 = diff_in_second(start, end);
+
+    printf("execution time of append() : %lf sec\n", cpu_time1);
+    printf("execution time of findName() : %lf sec\n", cpu_time2);
+
+    /* FIXME: release all allocated entries */
+    Release_Hash_Table();
+
+    return 0;
+}
+#endif
+
+#ifdef ORIGINAL
 int main(int argc, char *argv[])
 {
     FILE *fp;
@@ -35,12 +90,10 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    init();
-
     /* build the entry */
     entry *pHead, *e;
     pHead = (entry *) malloc(sizeof(entry));
-    printf("size of entry : %lu bytes\n", sizeof(HashList));
+    printf("size of entry : %lu bytes\n", sizeof(entry));
     e = pHead;
     e->pNext = NULL;
 
@@ -53,7 +106,7 @@ int main(int argc, char *argv[])
             i++;
         line[i - 1] = '\0';
         i = 0;
-        pushHashTable(line,HashFunc(line));
+        e = append(line, e);
     }
     clock_gettime(CLOCK_REALTIME, &end);
     cpu_time1 = diff_in_second(start, end);
@@ -63,18 +116,20 @@ int main(int argc, char *argv[])
 
     e = pHead;
 
-    /* the given last name to find */
-    char input[MAX_LAST_NAME_SIZE] = "zzzzzzzz";
+    /* the givn last name to find */
+    char input[MAX_LAST_NAME_SIZE] = "zyxel";
     e = pHead;
+
+    assert(findName(input, e) &&
+           "Did you implement findName() in " IMPL "?");
+    assert(0 == strcmp(findName(input, e)->lastName, "zyxel"));
 
 #if defined(__GNUC__)
     __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
 #endif
     /* compute the execution time */
     clock_gettime(CLOCK_REALTIME, &start);
-    //findName(input, e);
-    int found=IsFound(input);
-    assert(found==1);
+    findName(input, e);
     clock_gettime(CLOCK_REALTIME, &end);
     cpu_time2 = diff_in_second(start, end);
 
@@ -83,6 +138,7 @@ int main(int argc, char *argv[])
 
     /* FIXME: release all allocated entries */
     free(pHead);
-    FREE_HashTable();
+
     return 0;
 }
+#endif
