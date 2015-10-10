c342d267f6798264cafd9cef5d553fd5d1403729
Yuning Lin <pandaning9112@gmail.com>
2015-10-10 17:18:07 +0800

    Modify parameter
    Modify the parameter MAX_LAST_NAME_SIZE from 16 to 15, and use it in
    char name[ ], because the max length of the name in all-names.txt is 14.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -1,14 +1,14 @@
 #ifndef _PHONEBOOK_H
 #define _PHONEBOOK_H
 
-#define MAX_LAST_NAME_SIZE 16
+#define MAX_LAST_NAME_SIZE 15
 
 #define method  2      //Choose which hash function to use.
 #define HashTableSize 69997 //a prime number
 
 /* Revised version */
 typedef struct  HashList {
-    char name[13];
+    char name[MAX_LAST_NAME_SIZE];
     struct HashList *next;
 } HashList;
 
