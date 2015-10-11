afc70adb93d4b5c3f792af2e35f94d7b45d153ee
Yuning Lin <pandaning9112@gmail.com>
2015-10-11 15:29:23 +0800

    Use marco to define HashFunc
    Use marco to define Hashfunc instead of use switch case.
    
    Delete useless parameter in function “push_Hash_Table”.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,9 +2,8 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 15
-
-#define method  2      //Choose which hash function to use.
-#define HashTableSize 69997 //a prime number
+#define HashFunc sdbm //Choose which hash function to use.
+#define HashTableSize 6997 //a prime number
 
 /* Revised version */
 typedef struct  HashList {
@@ -16,8 +15,7 @@ HashList *head[HashTableSize];
 HashList *ptr[HashTableSize];
 
 void init_Hash_Table();
-int HashFunc(char *str);//return pos;
 int IsFound(char *str);
-void push_Hash_Table(char *str,int id);
+void push_Hash_Table(char *str);
 void Release_Hash_Table();
 #endif
