b953ca3282740b037224aeba4178d5fe6fe51dce
Yuning Lin <pandaning9112@gmail.com>
2015-10-12 19:06:18 +0800

    Implementing abstraction with C
    Implementing abstraction with C by using struct, it can change
    functions to useful API, and very convenient to use in the future.
    
    Delete function call below.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,7 +2,7 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 15
-#define HashFunc sdbm //Choose which hash function to use.
+#define HashFunc djb2 //Choose which hash function to use.
 #define HashTableSize 6997 //a prime number
 
 /* Revised version */
@@ -11,11 +11,14 @@ typedef struct  HashList {
     struct HashList *next;
 } HashList;
 
+struct phonebook {
+    void (*init_Hash_Table)(void);
+    int (*IsFound)(char *str);
+    void (*push_Hash_Table)(char *str);
+    void (*Release_Hash_Table)(void);
+};
+
 HashList *head[HashTableSize];
 HashList *ptr[HashTableSize];
-
-void init_Hash_Table();
-int IsFound(char *str);
-void push_Hash_Table(char *str);
-void Release_Hash_Table();
+struct phonebook phonebook;
 #endif
